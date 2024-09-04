86a5ce7a0452fe74bc60105ca625c04a7b89e149
Luiz Hoffmann <luiz.hoffmann.leineker@gmail.com>
2024-09-04 11:32:49 -0300

    aula 04

---
+++ API/WEB/Models/Produto.cs
@@ -0,0 +1,48 @@
+﻿
+namespace WEB.Models;
+
+
+public class Produto{ 
+
+    public Produto(){
+        CriadoEm = DateTime.Now;
+        Id = Guid.NewGuid().ToString();
+
+
+    }
+
+
+
+    //PUBLICO
+    public string? Id {get; set;}
+    public string? Nome {get; set;}
+    public double Preco {get; set;}
+
+    public int Quantidade {get; set;}
+
+    public DateTime CriadoEm { get; set; }  
+    
+
+
+
+
+
+
+
+    /*
+    PRIVATE
+
+    private double preco;
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(double preco){
+        this.preco = preco * 2.5;
+    }
+
+
+*/
+}
+
--- API/WEB/Program.cs
+++ API/WEB/Program.cs
@@ -1,8 +1,10 @@
 //Testar API: Rest Client - Extensão do VSCODE, SOFTWARE: Postman, Insomnia
+using WEB.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!");
+app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/user", () => "USER: H!!");
 
@@ -16,6 +18,36 @@ app.MapGet("/retornar", () =>
     return endereco;
 });
 
+List<Produto> produtos =
+[
+    new Produto(){
+        Nome = "Notebook",
+        Preco = 5000,
+        Quantidade = 54
+},
+    new Produto(){
+        Nome = "Computador",
+        Preco = 2900,
+        Quantidade = 16
+},
+    new Produto(){
+        Nome = "Batata",
+        Preco = 300,
+        Quantidade = 6
+},
+];
+
+//GET
+app.MapGet("/produto/list", () => {
+    return Results.Ok(produtos);
+});
+
+app.MapPost("/produto/cadastrar/{nome}", (string nome) => {
+    Produto produto = new Produto();
+    produto.Nome = nome;
+    produtos.Add(produto);
+    return Results.Ok(produtos);
+});
 
 //Criar um Funcionalidade para receber informação
 // Receber Informação pela URL da req
@@ -24,3 +56,11 @@ app.MapGet("/retornar", () =>
 
 
 app.Run();
+
+/*Produto produto = new Produto();
+
+produto.Preco = 5;
+
+Console.WriteLine("Preço: " + produto.Preco);
+
+*/
